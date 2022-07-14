a4075a174662fe5aa5be20581593361cdb6cb1f6
aorri2 <aorri96@ikw.kr>
2022-07-14 14:40:45 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -20,8 +20,14 @@ public class Category {
     @ManyToMany
     @JoinTable(name="category_item",
             joinColumns = @JoinColumn(name="category_id"),
-                inverseJoinColumns = @JoinColumn(name = "item_id")
-    )
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items= new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
 
 }
